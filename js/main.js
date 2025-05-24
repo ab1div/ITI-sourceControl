bf6cc4de7af0076343f978fec989074435d6b598
Abdelrahman Mahmoud <156802584+ab1div@users.noreply.github.com>
2025-05-24 14:10:45 +0300

    add project files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("my-btn");
+
+function btnAlert(){
+    button.addEventListener("click", ()=>{
+    alert(`welcome to Git environment 
+        Second edit to js file`);
+})
+} 
